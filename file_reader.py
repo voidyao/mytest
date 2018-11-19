5fba61aeed9c4b6767f99487ce03b1ee9c6a9bb0
voidyao <yaominghe@hotmail.com>
2018-11-19 16:18:44 +0800

    1

--- file_reader.py
+++ file_reader.py
@@ -1,3 +1,8 @@
-with open('pi_digits.txt') as file_object:
-    contents = file_object.read()
-    print(contents)
+# with open('pi_digits.txt') as file_object:
+#     contents = file_object.read()
+#     print(contents.strip())
+filename = 'pi_digits.txt'
+
+with open(filename) as file_object:
+    for line in file_object:
+        print(line)
