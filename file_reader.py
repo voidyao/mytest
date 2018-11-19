5783629a2c12b217a5c41eb017a0e31e6800618e
voidyao <yaominghe@hotmail.com>
2018-11-19 16:49:49 +0800

    1

--- file_reader.py
+++ file_reader.py
@@ -4,5 +4,10 @@
 filename = 'pi_digits.txt'
 
 with open(filename) as file_object:
-    for line in file_object:
-        print(line.strip())
+    lines =  file_object.readlines()
+
+pi_string = ''
+for line in lines:
+    pi_string += line.strip()
+print(pi_string)
+print(len(pi_string))
